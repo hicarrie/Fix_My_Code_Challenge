59dc61f93feeb9eeeec72cc596b4efa7ef927387
Carrie Ybay <carrieybay@gmail.com>
2017-09-19 19:08:38 -0700

    Edit documentation and clean up method names

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,28 +1,34 @@
 #!/usr/bin/python3
+"""
+Defines Square class
+"""
 
-class square():
-    
+
+class Square():
+    """ attributes and methods for Square objects """
+
     width = 0
     height = 0
 
-    
     def __init__(self, *args, **kwargs):
+        """ initializes Square object """
         for key, value in kwargs.items():
             setattr(self, key, value)
 
-    def area_of_my_square(self):
-        """ Area of the square """
+    def area(self):
+        """ area of the square """
         return self.width * self.width
 
-    def PermiterOfMySquare(self):
+    def perimeter(self):
+        """ perimeter of the square """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """ string representation of the square """
         return "{}/{}".format(self.width, self.height)
 
 if __name__ == "__main__":
-
-    s = square(width=12, height=9)
+    s = Square(width=12, height=9)
     print(s)
-    print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.area())
+    print(s.perimeter())
